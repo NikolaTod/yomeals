f73656ad55f501075bf32e2e628e26256fb4432d
Nikola Todorovic <nikola96todor@gmail.com>
2021-10-11 20:26:03 +0200

    Created BasicPage

---
+++ src/pages/BasicPage.java
@@ -0,0 +1,15 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+
+public class BasicPage {
+
+	private WebDriver driver;
+	
+	public BasicPage(WebDriver driver) {
+		this.driver = driver;
+	}
+	
+	
+}
