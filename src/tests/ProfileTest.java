e4627dbe8e75a9bcaa6007ad5783ca2226949b5e
Nikola Todorovic <nikola96todor@gmail.com>
2021-10-13 15:46:02 +0200

    Added change profile image test

--- src/tests/ProfileTest.java
+++ src/tests/ProfileTest.java
@@ -1,5 +1,8 @@
 package tests;
 
+import java.io.File;
+import java.io.IOException;
+
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -21,15 +24,41 @@ public class ProfileTest extends BasicTest {
 		String zipCode = "12";
 		String country = "India";
 		String state = "Goa";
-		String city = "Raia";	
+		String city = "Raia";
 		profilePage.changeInfo(firstName, lastName, address, phone, zipCode, country, state, city);
-		
+
 		Assert.assertTrue(notificationSystemPage.getMessageText().contains("Setup Successfu"), "Error: setup");
-		
+
 		authPage.logout();
-		
-		Assert.assertTrue(notificationSystemPage.getMessageText().contains("Logout Successfull!"), "Error: logout");			
+
+		Assert.assertTrue(notificationSystemPage.getMessageText().contains("Logout Successfull!"), "Error: logout");
 	}
-	
-	
+
+	@Test
+	public void changeProfileImage() throws InterruptedException, IOException {
+		driver.get(baseUrl + "guest-user/login-form");
+		locationPopupPage.closePopup();
+		loginPage.login(email, password);
+
+		Assert.assertTrue(notificationSystemPage.getMessageText().contains("Login Successfull"), "Error: login");
+
+		driver.get(baseUrl + "member/profile");
+		String imgPath = new File("img/profile_pic.jpg").getCanonicalPath();
+		profilePage.uploadPic(imgPath);
+
+		Assert.assertTrue(notificationSystemPage.getMessageText().contains("Profile Image Uploaded Successfully"),
+				"Error: login");
+
+		notificationSystemPage.waitForMsgToDissapear();
+		profilePage.removePic();
+
+		Assert.assertTrue(notificationSystemPage.getMessageText().contains("Profile Image Deleted Successfully"),
+				"Error: login");
+		notificationSystemPage.waitForMsgToDissapear();
+		authPage.logout();
+
+		Assert.assertTrue(notificationSystemPage.getMessageText().contains("Logout Successfull!"), "Error: login");
+
+	}
+
 }
